456e302120914d0f46d308dc6b0958ed8da9f7d0
Lorenzo Della Penna <lorenzo.dellapenna@gmail.com>
2016-09-11 19:29:51 +0200

    poco niente
    adadas

--- js/actions.js
+++ js/actions.js
@@ -5,23 +5,3 @@ $(document).on({
         $body.addClass("loading");    },
     ajaxStop: function() { $body.removeClass("loading"); }
 });
-
-$( document ).ready(function() {
-    //loads header
-    $.ajax({
-        type:'GET',
-        url: "http://localhost/html/header.html",
-        crossDomain: true,
-    }).success(function(result){
-        $("#header").html(result);
-    });
-    
-    //loads footer
-    $.ajax({
-        type:'GET',
-        url: "http://localhost/html/footer.html",
-        crossDomain: true,
-    }).success(function(result){
-        $("#footer").html(result);
-    });
-});
