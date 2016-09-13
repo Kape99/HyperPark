ef00ed371783ba71ca904f3a695f1b72b84d9297
Emanuele Chioso <emanuele.chioso@gmail.com>
2016-09-13 19:23:52 +0200

    working on js and php," 'mbareee che ci devi fare?? "

---
+++ php/park.php
@@ -0,0 +1,22 @@
+<?php
+//$servername = "ftp.parcomajella.altervista.org";
+//$username = "parcomajella";
+$servername = "localhost";
+$username = "user";
+$password = capeculo0;
+$dbname = "my_parcomajella";
+
+// Create connection
+$mysqli = new mysqli($servername, 'root', 'root', $dbname);
+
+//$query = "SELECT *  FROM  PERCORSO  WHERE DIFFICOLTA='".$_POST['diff']."'";
+$query="SELECT ID, NOME FROM PERCORSO WHERE 1";
+$result = $mysqli->query($query);
+$array = array();
+    while($row = mysqli_fetch_array($result)) {
+        array_push($array, $row);
+       echo explode(" ",$array);
+    }
+    echo json_encode(array_values($array));;
+$mysqli->close();
+?>
