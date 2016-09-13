d12cbdde91209de480f6264af100c36cbf77b57d
Emanuele Chioso <emanuele.chioso@gmail.com>
2016-09-13 16:28:40 +0200

    js footer and header

--- js/park.js
+++ js/park.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function($){
-        $("#header").load("header.html");
-        $("#footer").load("footer.html");
+        $('header').load("./header.html");
+        $('footer').load("./footer.html");
     });
+
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,
+    function(m,key,value) {
+      vars[key] = decodeURIComponent(value);
+    });
+    return vars;
+  }
